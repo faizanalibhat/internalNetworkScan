609fca8724451c778d822ff50b0c4ad54515e6b3
Faizan Bhat <faizan@snapsec.co>
2024-06-05 06:57:37 +0000

    "Regular Backup"

--- tools/installTools.sh
+++ tools/installTools.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Initialize a Go module
+go mod init internalNetworkScan
+
 # Install nmap
 sudo apt-get update
 sudo apt-get install -y nmap
 
 # Install httpx
-GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
+GO111MODULE=on go install github.com/projectdiscovery/httpx/cmd/httpx@latest
 sudo cp ~/go/bin/httpx /usr/local/bin/
 
 # Install nuclei
-GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on go install github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
 sudo cp ~/go/bin/nuclei /usr/local/bin/
