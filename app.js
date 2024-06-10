132c60498d077b271ad583ac7e291ec500e738e0
Faizan Bhat <faizan@snapsec.co>
2024-06-10 07:25:45 +0000

    "Regular Backup"

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', scanRoutes);
 
-app.listen(config.port, () => {
+app.listen(config.port, "0.0.0.0", () => {
     console.log(`Internal network scanning API running on port ${config.port}`);
 });
