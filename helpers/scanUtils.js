eacbd86342cfc515dde362e3ca6d75890d67935f
Faizan Bhat <faizan@snapsec.co>
2024-06-14 12:08:25 +0000

    "Regular Backup"

--- helpers/scanUtils.js
+++ helpers/scanUtils.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const fs = require('fs');
 
 /**
  * Perform a Nuclei scan on the given IP address.
